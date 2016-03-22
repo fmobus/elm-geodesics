c793d256533782d39fd0a99e6a4086b0d92d1052
Felipe Mobus <fmobus@thoughtworks.com>
2016-03-22 01:51:43 -0300

    adding documentation and credits to other authors

---
+++ README.md
@@ -0,0 +1,9 @@
+Elm implementation of geographic coordinates Latitude/Longitude for elm
+
+The implementation of this library is based on the work of Chris Veness[1], who converted
+the haversine elliptical formulae into javascript, and also on the work of Peter Haldbæk[2],
+who implemented the javascript mt-latlon library.
+
+[1] http://www.movable-type.co.uk/scripts/latlong.html
+[2] https://github.com/peterhaldbaek/mt-latlon
+
--- src/Geodesics.elm
+++ src/Geodesics.elm
@@ -4,6 +4,13 @@ module Geodesics(LatLon, latLon, distance, moveWithBearing) where
 
 @docs LatLon, latLon, distance, moveWithBearing
 
+The implementation of this library is based on the work of Chris Veness[1], who converted
+the haversine elliptical formulae into javascript, and also on the work of Peter Haldbæk[2],
+who implemented the javascript mt-latlon library.
+
+[1] http://www.movable-type.co.uk/scripts/latlong.html
+[2] https://github.com/peterhaldbaek/mt-latlon
+
 -}
 
 {-| Represents a Latitude/Longitude coordinate in a geodesic system -}
